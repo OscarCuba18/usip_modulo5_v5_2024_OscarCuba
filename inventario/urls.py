c1647c3ea7d61d59400f7396ab997be8fcaf84b7
OscarCuba18 <ocubac99@gmail.com>
2024-07-12 16:13:00 -0400

    Ejercicio 1 commit: Add filter name search by category into views.py and displayed into a new template.

--- inventario/urls.py
+++ inventario/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('contact/<str:name>/', views.contact, name='contact'),
-    path('categorias/', views.categorias, name='categorias'),
+    #path('categorias/', views.categorias, name='categorias'),
+    # http://127.0.0.1:8000/inventario/categorias?nombre=PALABRA_A_FILTRAR
+    path('categorias/', views.filtrar_categorias, name='filtrar_categorias'),
     path('saludo/', views.index)
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -14,3 +14,12 @@ def categorias(request):
     print(filtro_nombre)
     items = Categoria.objects.all()
     return render(request, "categorias.html", {"categorias": items})
+
+def filtrar_categorias(request):
+    nombre_filtro = request.GET.get('nombre', None)
+    print(nombre_filtro)
+    if nombre_filtro:
+        categorias = Categoria.objects.filter(nombre__icontains=nombre_filtro)
+    else:
+        categorias = Categoria.objects.all()
+    return render(request, 'categorias_list.html', {'categorias': categorias})
